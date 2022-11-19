4fd366c64f9b1576cbcdcd04895c76b46f78e307
Cpaluszek <105011036+Cpaluszek@users.noreply.github.com>
2022-11-19 11:43:16 +0100

    Update

--- get_next_line.c
+++ get_next_line.c
@@ -5,34 +5,103 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: cpalusze <cpalusze@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/16 11:39:24 by cpalusze          #+#    #+#             */
-/*   Updated: 2022/11/18 20:39:53 by test_user        ###   ########.fr       */
+/*   Created: 2022/11/19 10:45:03 by cpalusze          #+#    #+#             */
+/*   Updated: 2022/11/19 10:45:03 by cpalusze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stddef.h>
 
-// TODO: return error message
+static char	*read_file(int fd, char *buffer);
+static char	*ft_join_to_buffer(char *buffer, char *read);
+static char	*ft_get_line(char *buffer);
+static char	*ft_remove_current_line(char *buffer);
 
-// READ: if return 0 -> EOF
+// NOTE ?: return error message
 char	*get_next_line(int fd)
+{
+	static char	*buffer;
+	char		*line;
+
+	if (fd == -1 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = read_file(fd, buffer);
+	if (buffer == NULL)
+		return (NULL);
+	line = ft_get_line(buffer);
+	buffer = ft_remove_current_line(buffer);
+	return (line);
+}
+
+static char	*read_file(int fd, char *buffer)
 {
 	char	*content;
 	size_t	byte_count;
-	
-	if (fd == -1)
-		return (NULL);
-	content = (char *) malloc(sizeof(char) * BUFFER_SIZE);
+
+
+	content = (char *) malloc(sizeof(char) * BUFFER_SIZE + 1);
 	if (content == NULL)
 		return (NULL);
-	byte_count = read(fd, content, BUFFER_SIZE);
-	if (byte_count == -1)
+	byte_count = 1;
+	while (byte_count > 0)
+	{
+		byte_count = read(fd, content, BUFFER_SIZE);
+		if (byte_count == -1)
+		{
+			free(content);
+			return (NULL);
+		}
+		content[byte_count] = '\0';
+		buffer = ft_join_to_buffer(buffer, content);
+		if (ft_strchr(buffer, '\n'))
+			break ;
+	}
+	free(content);
+	return (buffer);
+}
+
+static char	*ft_join_to_buffer(char *buffer, char *read)
+{
+	char	*join;
+
+	join = ft_strjoin(buffer, read);
+	free(buffer);
+	return (join);
+}
+
+// NOTE: check if first char is not '\0' ?
+// NOTE: if eol is \0 or \n, replace eol by \n ?
+static char	*ft_get_line(char *buffer)
+{
+	char	*line;
+	size_t	i;
+
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	line = ft_substr(buffer, 0, i + 1);
+	if (line == NULL)
 		return (NULL);
-	// TODO: split tring until next \n
-	// Keep the remaining 
-	// Recover remaining ? (strlcat ?)
-	// realloc may be useful
-	// return line
-	return (content);
+	return (line);
+}
+
+static char	*ft_remove_current_line(char *buffer)
+{
+	int		i;
+	int		j;
+	char	*remaining;
+
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	if (buffer[i] == '\0')
+	{
+		free(buffer);
+		return (NULL);
+	}
+	remaining = ft_substr(buffer, i, ft_strlen(buffer) - i);
+	if (remaining == NULL)
+		return (NULL);
+	free(buffer);
+	return (remaining);
 }
---
+++ get_next_line_utils.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cpalusze <cpalusze@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/19 10:46:56 by cpalusze          #+#    #+#             */
+/*   Updated: 2022/11/19 11:32:05 by cpalusze         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	char	ch;
+
+	ch = (unsigned char) c;
+	while (*s && *s != ch)
+		s++;
+	if (*s == ch)
+		return ((char *)s);
+	return (NULL);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	len1;
+	size_t	len2;
+	char	*join;
+
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	if (s1 == NULL)
+		return (ft_strdup(s2));
+	if (s2 == NULL)
+		return (ft_strdup(s1));
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	join = (char *) malloc(sizeof(char) * (len1 + len2 + 1));
+	if (join == NULL)
+		return (NULL);
+	join[0] = '\0';
+	ft_strlcat(join, s1, len1 + 1);
+	ft_strlcat(join, s2, len1 + len2 + 1);
+	return (join);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+	size_t	i;
+
+	if (s == NULL)
+		return (NULL);
+	if (ft_strlen(s) <= start)
+		len = 0;
+	else if (ft_strlen(s) < start + len)
+		len = ft_strlen(s) - start;
+	sub = (char *) malloc(sizeof(char) * len + 1);
+	if (sub == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	sub[i] = '\0';
+	return (sub);
+}
